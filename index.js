4480b849d3dc15b361fcc6f0b349fcedb87cb96c
Mattinik <148599697+Mattinik@users.noreply.github.com>
2023-12-13 23:47:54 +0800

    Update index.js
    Add functionality for signup and log in

--- index.js
+++ index.js
@@ -1,107 +1,13 @@
-
-
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    if (validateInputs()) {
-        redirectToLoginForm();
-    }
-});
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success');
-};
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-};
-
-const isValidPassword = password => {
-    const pattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return pattern.test(String(password));
-};
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if (usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
-    }
-
-    if (emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
-    }
-
-    if (passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8) {
-        setError(password, 'Password must be at least 8 characters.');
-    } else if (!isValidPassword(passwordValue)) {
-        setError(password, 'Password should contain at least one uppercase letter, one digit, and one special character.');
-    } else {
-        setSuccess(password);
-    }
-
-    if (password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords don't match");
-    } else {
-        setSuccess(password2);
-    }
-
-    return document.querySelectorAll('.success').length === 4;
-
-};
-
-const redirectToLoginForm = () => {
-    const signUpModal = new bootstrap.Modal(document.getElementById('Modal_SU'));
-    signUpModal.hide();
-
-    const logInModal = new bootstrap.Modal(document.getElementById('Modal_LI'));
-    logInModal.show();
-};
+//--SignUp and LogIn --//
+function signUp() {
+    window.location.href = "signup.html";
+}
 
 
 
-
-
-
-// --Log In--//
-
-
-
+function logIn() {
+    window.location.href = "login.html";
+}
 
 
 //--Favorite--//
