38a8de3e87ce2047f02e342304e4b27659da5411
Mattinik <148599697+Mattinik@users.noreply.github.com>
2023-12-21 17:33:04 +0800

    Update signup.js
    Updated validation.

--- signup.js
+++ signup.js
@@ -1,12 +1,10 @@
-// Additional security checks for email and password
+//security checks for email and password
 const isValidEmail = email => {
-    // Use a more secure regex for email validation
     const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return re.test(String(email).toLowerCase());
 };
 
 const isValidPassword = password => {
-    // Enhance password security requirements
     const pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&^#])[A-Za-z\d@$!%*?&^#]{8,}$/;
     return pattern.test(String(password));
 };
@@ -45,43 +43,49 @@ const validateInputs = (username, email, password, password2) => {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
+    let isValid = true;
+
     if (usernameValue === '') {
         setError(username, 'Username is required');
+        isValid = false;
     } else {
         setSuccess(username);
     }
 
     if (emailValue === '') {
         setError(email, 'Email is required');
+        isValid = false;
     } else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
+        isValid = false;
     } else {
         setSuccess(email);
     }
 
     if (passwordValue === '') {
         setError(password, 'Password is required');
+        isValid = false;
     } else if (passwordValue.length < 8 || !isValidPassword(passwordValue)) {
         setError(
             password,
             'Password should be at least 8 characters and contain at least one uppercase letter, one digit, and one special character.'
         );
+        isValid = false;
     } else {
         setSuccess(password);
     }
 
     if (password2Value === '') {
         setError(password2, 'Please confirm your password');
+        isValid = false;
     } else if (password2Value !== passwordValue) {
         setError(password2, "Passwords don't match");
+        isValid = false;
     } else {
         setSuccess(password2);
-
-        window.location.href = 'login.html'; 
-        return true;
     }
 
-    return false;
+    return isValid;
 };
 
 
@@ -115,9 +119,17 @@ const saveData = () => {
 // Event listener for form submission
 document.getElementById('signupForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    saveData(); 
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const password2Input = document.getElementById('password2');
+
+    if (validateInputs(usernameInput, emailInput, passwordInput, password2Input)) {
+        saveData();
+        window.location.href = 'login.html';
+    }
 });
 
 document.getElementById('login_btn').addEventListener('click', function () {
-    window.location.href = 'login.html'; 
+    window.location.href = 'login.html';
 });
